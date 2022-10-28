4b62642c6c4aac503fb860e92f5f9ca137fc7df3
cutekoble <claireanangilan@gmail.com>
2022-10-28 15:38:44 +0800

    Changed of Profile

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,9 +8,9 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets-homepage/assets/img/avataaars.svg" alt="..." />
+                <img class="masthead-avatar mb-5" src="assets-homepage/assets/img/cat.png" alt="..." />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">The Pussy Cat</h1>
+                <h1 class="masthead-heading text-uppercase mb-0" style="color:#e75480;" >The Pussy Cat</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -18,7 +18,7 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+                <p class="masthead-subheading font-weight-light mb-0">Purrfect just the way you are </p>
             </div>
         </header>
         <!-- Portfolio Section-->
